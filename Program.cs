e16b3f4c21d0a036d8d544d6fcd9c0c57734d3a8
marcelldac <dacmarcell@gmail.com>
2023-10-08 17:41:55 -0300

    feat: adiciona metodo start

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch{
+    class Program{
+        static void Main(string[] args){
+            Start(10);
+        }
+
+        static void Start(int time){
+            int currentTime = 0;
+            while(time != currentTime){
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Contador finalizado, voltando pro menu...");
+            Thread.Sleep(2500);
+        }
+    }
+}
